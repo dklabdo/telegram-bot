4b467a44dbc5c071efb24e7bb91b1aea46c67408
dklabdo <sayah.abdo22@gmail.com>
2025-01-18 23:39:01 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,6 @@
-"use client";
-import { useState } from "react";
-import img1 from "../../public/img1.png";
-import img2 from "../../public/img2.png";
-import icon from "../../public/icon.svg";
-import m1 from "../../public/medal1.png";
-import m2 from "../../public/medal2.png";
-import m3 from "../../public/medal3.png";
-import { ChevronRight, CircleCheckBig, CopyIcon } from "lucide-react";
-import Image from "next/image";
+
+
 export default function Home() {
-  const [btn, setBtn] = useState(1);
   return (
     <>
       <main className="w-full  text-white p-4 flex justify-center items-center h-screen">
